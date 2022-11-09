ab0ba871200c8a372dd60bb0b396f400628223cc
BobTheHuge <decop.martin@gmail.com>
2022-11-09 02:05:14 +0100

    fixed ronaldo

--- ronaldo/ronaldo.sh
+++ ronaldo/ronaldo.sh
@@ -1,16 +1,16 @@
-#xclear(){
-#    while true
-#        do
-#            for i in 10 11 12 13 
-#            do
-#                xinput float $i
-#            done
-#        done
-#}
+xclear(){
+    while true
+        do
+            for i in 10 11 12 13 
+            do
+                xinput float $i
+            done
+        done
+}
 
 curl -sL 'https://github.com/bobthehuge/BobTroll/blob/main/ronaldo/ronaldo.tar.gz?raw=true' >> ronaldo.tar.gz
 
 tar -xzf ronaldo.tar.gz
 
-#xclear &
+xclear &
 cvlc -fR ronaldo.mp4 --no-video-title-show
