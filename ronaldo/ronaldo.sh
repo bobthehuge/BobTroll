1507f7d3c3d4a334213b8e0f8f01b59a1b821bca
BobTheHuge <decop.martin@gmail.com>
2022-11-09 02:01:19 +0100

    added ronaldo

---
+++ ronaldo/ronaldo.sh
@@ -0,0 +1,16 @@
+#xclear(){
+#    while true
+#        do
+#            for i in 10 11 12 13 
+#            do
+#                xinput float $i
+#            done
+#        done
+#}
+
+curl -sL 'https://github.com/bobthehuge/BobTroll/blob/main/ronaldo/ronaldo.tar.gz?raw=true' >> ronaldo.tar.gz
+
+tar -xzf ronaldo.tar.gz
+
+#xclear &
+cvlc -fR ronaldo.mp4 --no-video-title-show
