0514d7f6e991df85c3a908f5134c7300f173c7e6
bobthehuge <bobthehuge@protonmail.com>
2025-09-05 19:31:47 +0200

    fixed broken links and onelined loops

--- shrek/shrek.sh
+++ shrek/shrek.sh
@@ -1,16 +1,7 @@
-xclear(){
-    while true
-        do
-            for i in 10 11 12 13 
-            do
-                xinput float $i
-            done
-        done
+xclear() {
+  while true; do for i in 10 11 12 13; do xinput float $i; done; done
 }
-
-curl -sL '' >> shrek.tar.gz
-
+curl -sL 'https://github.com/bobthehuge/BobTroll/blob/main/shrek/shrek.tar.gz?raw=true' >> shrek.tar.gz
 tar -xzf shrek.tar.gz
-
 xclear &
 cvlc -fR shrek.mp4
