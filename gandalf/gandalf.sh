814f4e2e4dace74f859960952c0bd6ab42f66814
bobthehuge <bobthehuge@protonmail.com>
2025-09-05 19:37:14 +0200

    forgot gandalf...

--- gandalf/gandalf.sh
+++ gandalf/gandalf.sh
@@ -1,16 +1,7 @@
-xclear(){
-    while true
-        do
-            for i in 10 11 12 13 
-            do
-                xinput float $i
-            done
-        done
+xclear() {
+  while true; do for i in 10 11 12 13; do xinput float $i; done; done
 }
-
-curl -sL 'https://github.com/bobthehuge/gandalf/blob/main/gandalf.mp4?raw=true' >> gandalf.tar.gz
-
+curl -sL 'https://github.com/bobthehuge/BobTroll/blob/main/gandalf/gandalf.tar.gz?raw=true' >> gandalf.tar.gz
 tar -xzf gandalf.tar.gz
-
 xclear &
 cvlc -fR gandalf.mp4
